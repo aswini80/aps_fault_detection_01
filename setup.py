3c13ffff6ea8ad9af844c9c95d76b8ce5961cb34
aswini80 <aswinikk@gmail.com>
2024-01-03 07:29:17 +0530

    3rd update

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
 from setuptools import find_packages, setup
+from typing import List
+
+REQUIREMENT_FILE_NAME = "requirements.txt"
+HYPHON_E_DOT = "-e ."
 
 setup(
     name = "sensor",
@@ -9,5 +13,11 @@ setup(
     install_reqires = get_requirements()
 )
 
-def get_requirements():
-    pass
+def get_requirements()->List[str]:
+    with open(REQUIREMENT_FILE_NAME) as requiremnt_file:
+        requirement_list = requiremnt_file.readlines()
+        requirement_list = [requirement_name.replace("\n", "") for requiremet_name in requirement_list]
+    if HYPHON_E_DOT in requirement_list:
+        requirement_list.remove(HYPHON_E_DOT)
+    return requirement_list
+    
